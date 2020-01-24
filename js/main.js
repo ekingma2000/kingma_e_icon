047898c0bfc30a8d1ae784cac40cd08f67575742
Kingma <e_kingma@ats-hr420-dm21.fc.ca>
2020-01-24 11:22:01 -0500

    bomb click event

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 (() => {
 // this is a module pattern, also called iife
 
-allSVGs = document.querySelectorAll(".container");
+let allSVGs = document.querySelectorAll(".container");
+	bomb = document.querySelector("#bomb")
 
 function logSVG(){
     console.log(this.id);
+
+    this.classList.toggle('selected');
   }
 
 
 //set up our triggers here
 allSVGs.forEach(item => item.addEventListener("click", logSVG));
+bomb.addEventListener("click", logSVG)
 
 })();
