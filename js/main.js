b2cb2d9824cdd26e1496bcb177940d811229cbc0
Kingma <e_kingma@ats-hr420-dm21.fc.ca>
2020-01-24 09:57:49 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 (() => {
+// this is a module pattern, also called iife
+  const SVGs = document.querySelector('container');
+
+function toggleSelected() {
+//we want some kind of indecator
+//that someone has selected an icon
+
+  debugger;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+//set up our triggers here
+SVGs.addEventListener('click', toggleSelected);
 })();
