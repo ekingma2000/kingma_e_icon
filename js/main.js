b2ac3b6e1e2abbc1de6a3c3d708efe2b5f80af66
ekingma2000 <emilyannkingma2000@gmail.com>
2020-01-25 19:07:58 -0500

    log to console and display check mark

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 (() => {
 // this is a module pattern, also called iife
-
-const allSVGs = document.querySelector("#bomb, #boomer, #bow, #cap, #fire, #hammer, #hook, #ice, #potion, #sword");
+const bomb = document.querySelector("#bomb");
+	boomer = document.querySelector("#boomer");
+	bow = document.querySelector("#bow");
+	cap = document.querySelector("#cap");
+	fire = document.querySelector("#fire");
+	hammer = document.querySelector("#hammer");
+	hook = document.querySelector("#hook");
+	ice = document.querySelector("#ice");
+	potion = document.querySelector("#potion");
+	sword = document.querySelector("#sword");
 
 function logSVG(){
     console.log(this.id);
@@ -13,16 +21,25 @@ function toggleSelected(){
 }
 
 //set up our triggers here
-allSVGs.addEventListener("click", logSVG, toggleSelected);
-//bomb.forEach(item => item.addEventListener("click", logSVG))
-//boomer.forEach(item => item.addEventListener("click", logSVG))
-//bow.forEach(item => item.addEventListener("click", logSVG))
-//cap.forEach(item => item.addEventListener("click", logSVG))
-//fire.forEach(item => item.addEventListener("click", logSVG))
-//hammer.forEach(item => item.addEventListener("click", logSVG))
-//hook.forEach(item => item.addEventListener("click", logSVG))
-//ice.forEach(item => item.addEventListener("click", logSVG))
-//potion.forEach(item => item.addEventListener("click", logSVG))
-//sword.forEach(item => item.addEventListener("click", logSVG));
+bomb.addEventListener("click", logSVG);
+bomb.addEventListener("click", toggleSelected);
+boomer.addEventListener("click", logSVG);
+boomer.addEventListener("click", toggleSelected);
+bow.addEventListener("click", logSVG);
+bow.addEventListener("click", toggleSelected);
+cap.addEventListener("click", logSVG);
+cap.addEventListener("click", toggleSelected);
+fire.addEventListener("click", logSVG);
+fire.addEventListener("click", toggleSelected);
+hammer.addEventListener("click", logSVG);
+hammer.addEventListener("click", toggleSelected);
+hook.addEventListener("click", logSVG);
+hook.addEventListener("click", toggleSelected);
+ice.addEventListener("click", logSVG);
+ice.addEventListener("click", toggleSelected);
+potion.addEventListener("click", logSVG);
+potion.addEventListener("click", toggleSelected);
+sword.addEventListener("click", logSVG);
+sword.addEventListener("click", toggleSelected);
 
 })();
